5c592232c685211f2eb441f03d9022c62e448393
wlals1126 <a01040987587@gmail.com>
2022-05-02 22:07:01 +0900

    function

---
+++ js일정/js.js
@@ -0,0 +1,54 @@
+// // const a = 3;
+// // //변수
+// // var;//잘안씀
+// // let;
+
+// // //상수
+// // const;
+// let a = 3;
+// a = "문자열";
+// a = 3.1;
+// a = "a";
+
+// console.log(a);
+// if(1>0){
+//     console.log("1이 0보다 크다");
+// }else{
+//     console.log("0이 1보다 크다");
+// }
+// function 함수이름(매개이름);{
+//     실행문;
+//     return;
+// }
+
+// function add(a,b,c)
+// {
+//     // console.log(a+b+c);
+//     let result = a+b+c;
+//     return result;
+// }
+
+// let value add(3,4,5);
+// console.log(value);
+// fun1("sdf", "sdf");//입력문
+
+//a,b비교하는 함수
+// function 비교(a, b) {
+//   if (a == b) {
+//     console.log("a랑b가 같다");
+//   } else {
+//     console.log("a랑b가 다르다");
+//   }
+// }
+
+// 비교('a', 'a');
+
+//2개의 값을 받는다
+//1,5입력 = 1,2,3,4,5출력
+function func(a, b) {
+  for (let i = a; i <= b; i++) {
+    console.log(i);
+  }
+}
+
+func(1, 5);
